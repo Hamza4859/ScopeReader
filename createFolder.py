eff989e16e0cda146019f7c839e0255dd36fb2b6
Rtel Bennani <hamza.rtelbennani@dana.com>
2026-08-03 12:30:31 -0400

    removed test

--- createFolder.py
+++ createFolder.py
@@ -55,8 +55,3 @@ def create_test_report_directory(
     full_path.mkdir(parents=True, exist_ok=True)
 
     return full_path
-
-
-path = create_test_report_directory("10434039", "DEVELOP", "CO205A", "OverSpeed")
-
-print(path)
